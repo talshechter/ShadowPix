9d6662233f4248b78a1ed8ed4da5669132934cdb
Tal Shechter <42673060+talshechter@users.noreply.github.com>
2022-02-17 14:45:58 +0200

    Call generate mesh after the global method
    Also:
    1. replaced the global method images
    2. changed the number of iterations
    3. saved the loss trace to a file
    4. added a mesh.obj example

--- global_method_optimized.py
+++ global_method_optimized.py
@@ -1,22 +1,21 @@
 from image_utils import load_global_images
+from global_method_mesh_generator import generate_mesh
 import torch
 import numpy as np
 
 def heightfield_to_image_init(image_size, radius):
     """wrapper for the heighfield to image function."""
     def heightfield_to_image(heightfield):
-        """calculates the effective shaddow on every pixel and converts the heightfield to image."""
-        image = torch.zeros((image_size, image_size))
-        for j in range(image_size):
-            max_effective_shaddow_on_j = torch.zeros(image_size)
-            for k in range(1, radius+1):
-                if j+k >= image_size:
-                    break
-                pixel_effective_shaddow_on_j = heightfield[:, j+k] - k
-                max_effective_shaddow_on_j = torch.max(pixel_effective_shaddow_on_j, max_effective_shaddow_on_j)
-            image[:, j] = max_effective_shaddow_on_j - heightfield[:, j]
-        return image
-        
+      image = torch.zeros((image_size, image_size))
+      for j in range(image_size):
+        max_effective_shaddow_on_j = torch.full((1, image_size), -torch.inf)
+        for k in range(1, radius+1):
+          if j+k >= image_size:
+            break
+          pixel_effective_shaddow_on_j = heightfield[:, j+k] - k
+          max_effective_shaddow_on_j = torch.max(pixel_effective_shaddow_on_j, max_effective_shaddow_on_j)
+          image[:, j] = max_effective_shaddow_on_j - heightfield[:, j]
+      return image
     return heightfield_to_image
 
 
@@ -34,12 +33,12 @@ def compute_loss(images, heightfield, heightfield_to_image):
 
 def main():
     img1_path = 'global_images/img1.jpg'
-    img2_path = 'global_images/img2.png'
-    img3_path = 'global_images/img3.png'
-    img4_path = 'global_images/img4.png'
+    img2_path = 'global_images/img2.jpg'
+    img3_path = 'global_images/img3.jpg'
+    img4_path = 'global_images/img4.jpg'
     size = 50
     radius = 10
-    num_iterations = 1000
+    num_iterations = 10000
     images = load_global_images([img1_path, img2_path, img3_path, img4_path], size)
     device=torch.device('cpu')
     tensor_images = torch.stack([torch.from_numpy(image).to(device) for image in images]).float()
@@ -56,8 +55,9 @@ def main():
         optimizer.step()
         if i%100==0:
             loss_trace.append(loss)
-            print(loss)
-    print(loss_trace)
+    with open('optimized_global_loss_trace.txt', 'w') as f:
+        f.write(str(loss_trace))
+    generate_mesh(heightfield.detach(), 'optimized_global_method_mesh.obj')
 
 
 if __name__ == '__main__':
